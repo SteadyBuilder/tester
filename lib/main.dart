9e96be1e174e45188ae11bea74d2194845b9c514
Gomas <46985561+SteadyBuilder@users.noreply.github.com>
2025-03-28 14:49:55 +0900

    JsonDecodeExample

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,35 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tester/screen/post_screen.dart';
+import 'dart:convert';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp()));
+  runApp(const MaterialApp(
+    home: JsonDecodeExample(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class JsonDecodeExample extends StatelessWidget {
+  const JsonDecodeExample({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Riverpod + Dio 실습',
-      theme: ThemeData(
-          useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo)),
-      home: PostScreen(),
+    const jsonString =
+        '{"name": "SteadyBuidler", "age": 99, "job": "developer"}';
+
+    final Map<String, dynamic> userMap = jsonDecode(jsonString);
+
+    final name = userMap['name'];
+    final age = userMap['age'];
+    final job = userMap['job'];
+
+    return Scaffold(
+      appBar: AppBar(title: const Text("jsonDecode 예제")),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Text(
+          '이름: $name\n나이: $age\n직업: $job',
+          style: const TextStyle(fontSize: 20),
+        ),
+      ),
     );
   }
 }
--- lib/provider/post_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:tester/data/post_api.dart';
-import 'package:tester/model/post.dart';
-
-final postProvider = FutureProvider<Post>((ref) async {
-  return await postApi.fetchPost(1);
-});
